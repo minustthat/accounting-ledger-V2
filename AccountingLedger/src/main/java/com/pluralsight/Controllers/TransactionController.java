8f659aa1828253a1c92179899b1e18cc682a7669
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2024-07-03 01:31:19 -0700

    Configuration changes

--- AccountingLedger/src/main/java/com/pluralsight/Controllers/TransactionController.java
+++ AccountingLedger/src/main/java/com/pluralsight/Controllers/TransactionController.java
@@ -3,13 +3,18 @@ package com.pluralsight.Controllers;
 import com.pluralsight.DAO.TransactionDAO;
 import com.pluralsight.models.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+@RestController
+@CrossOrigin
+@RequestMapping("/transactions")
 
 public class TransactionController {
     private TransactionDAO transactionDAO;
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     public TransactionController(TransactionDAO transactionDAO) {
         this.transactionDAO = transactionDAO;
